611d6d8c294f4adde4f71759d229d497e0a95fcf
kplus87 <kplus87@gmail.com>
2016-08-16 21:46:13 -0500

    Add files via upload

---
+++ creds.py
@@ -0,0 +1,9 @@
+#Alexa
+Security_Profile_Description = "TheScrivener"
+Security_Profile_ID  = "amzn1.application.21a811c4248b41e484645877b7dc591c"
+Client_ID = "amzn1.application-oa2-client.2285b83f7d214ff7b3960126aa97eed0"
+Client_Secret = "6da338b5d6ca88522439d5235c1c4735239f551ae2cfd5b4af3dfba7079e9b7f"
+Product_ID = "ConCoEcho"
+
+#Redis
+redis_url = "https://sleepy-atoll-73625.herokuapp.com/code"
