60d8efbbe8c0f2f5be4ae99d06232d47098f22ce
kplus87 <kplus87@gmail.com>
2016-08-18 05:19:58 -0500

    Add files via upload

--- swipe.js
+++ swipe.js
@@ -13,33 +13,9 @@ function swipedetect(el, callback){
     startTime,
     handleswipe = callback || function(swipedir){}
   
-    touchsurface.addEventListener('click', function(e){
-      var touchobj = e.changedTouches[0]
-        swipedir = 'none'
-        dist = 0
-        startX = touchobj.pageX
-        startY = touchobj.pageY
-        startTime = new Date().getTime() // record time when finger first makes contact with surface
-        e.preventDefault()
-       setTimeout(function(){
-               var touchobj = e.changedTouches[0]
-        distX = touchobj.pageX - startX // get horizontal dist traveled by finger while in contact with surface
-        distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
-        elapsedTime = new Date().getTime() - startTime // get time elapsed
-        if (elapsedTime <= allowedTime){ // first condition for awipe met
-            if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ // 2nd condition for horizontal swipe met
-                swipedir = (distX < 0)? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
-            }
-            else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){ // 2nd condition for vertical swipe met
-                swipedir = (distY < 0)? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
-            }
-        }
-        handleswipe(swipedir)
-        e.preventDefault()
-       },3500);
-    }false)
+  
 
-    touchsurface.addEventListener('touchstart', function(e){
+    touchsurface.addEventListener('mouseover', function(e){
         var touchobj = e.changedTouches[0]
         swipedir = 'none'
         dist = 0
@@ -53,7 +29,7 @@ function swipedetect(el, callback){
         e.preventDefault() // prevent scrolling when inside DIV
     }, false)
   
-    touchsurface.addEventListener('touchend', function(e){
+    touchsurface.addEventListener('mouseout', function(e){
         var touchobj = e.changedTouches[0]
         distX = touchobj.pageX - startX // get horizontal dist traveled by finger while in contact with surface
         distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
