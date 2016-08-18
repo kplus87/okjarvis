0e6f13e670e9b62296d7a3495946c66a75513560
kplus87 <kplus87@gmail.com>
2016-08-18 16:52:46 -0500

    Add files via upload

--- swipe.js
+++ swipe.js
@@ -8,10 +8,12 @@ function swipedetect(el, callback){
     distY,
     threshold = 150, //required min distance traveled to be considered swipe
     restraint = 100, // maximum distance allowed at the same time in perpendicular direction
-    allowedTime = 300, // maximum time allowed to travel that distance
+    allowedTime = 0, // maximum time allowed to travel that distance
     elapsedTime,
     startTime,
     handleswipe = callback || function(swipedir){}
+
+  
 
   
 
